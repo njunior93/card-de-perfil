3af1b63d2cd8754bdbbafd04debbe5b63fcc904e
njunior93 <naeljunior07@gmail.com>
2025-12-10 18:40:14 -0300

    Ajuste nas posicoes dos botoes de seguir e mensagem no card

--- components/CardPerfil/CardPerfil.tsx
+++ components/CardPerfil/CardPerfil.tsx
@@ -52,7 +52,7 @@ export const CardPerfil = (props: iCardPerfil) =>{
         </ul>
       </div>
 
-      <div className="flex flex-col sm:flex-row gap-2">
+      <div className="flex flex-col gap-2">
         <Button
           onClick={() => setBtnSeguirStatus(!btnSeguirStatus)}
           className='bg-cyan-500 shadow-lg shadow-cyan-500/40'
